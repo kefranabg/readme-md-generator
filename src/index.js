b9cb34c0ce9d71a6a426fb14e367bc7131dfd524
NikxDa <NikxDa@users.noreply.github.com>
2019-06-15 23:08:30 +1200

    Command line improvements - Add --yes alias - Add help description

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ yargs
     args => mainProcess(args)
   )
   .boolean('yes')
+  .alias('y', 'yes')
+  .describe('yes', 'Use default values for all fields.')
   .help()
   .epilog(
     'for more information, find our manual at https://github.com/kefranabg/readme-md-generator'
