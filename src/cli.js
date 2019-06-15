37a68f3eed2c0d295e37b5baad139bd28250bee0
NikxDa <NikxDa@users.noreply.github.com>
2019-06-16 01:11:50 +1200

    Prettier fixes

--- src/cli.js
+++ src/cli.js
@@ -46,7 +46,9 @@ const getDefaultAnswer = question => {
     case 'input':
       return question.default || ''
     case 'checkbox':
-      return question.choices.filter(choice => choice.checked).map(choice => choice.value)
+      return question.choices
+        .filter(choice => choice.checked)
+        .map(choice => choice.value)
     default:
       return undefined
   }
