688c33833188a5487ff6df024d4993404ee0406c
Franck Abgrall <abgrallkefran@gmail.com>
2019-06-23 11:44:31 +0200

    :sparkles: Get author name from package.json even if author prop is an object (#75)

--- src/project-infos.spec.js
+++ src/project-infos.spec.js
@@ -272,5 +272,61 @@ describe('projectInfos', () => {
         testCommand: undefined
       })
     })
+
+    it('should return correct infos when author is defined as an object', async () => {
+      const packgeJsonInfos = {
+        name: 'readme-md-generator',
+        version: '0.1.3',
+        description: 'CLI that generates beautiful README.md files.',
+        author: {
+          name: 'Franck Abgrall',
+          email: 'abgrallkefran@gmail.com',
+          url: ''
+        },
+        license: 'MIT',
+        homepage: 'https://github.com/kefranabg/readme-md-generator',
+        repository: {
+          type: 'git',
+          url: 'git+https://github.com/kefranabg/readme-md-generator.git'
+        },
+        bugs: {
+          url: 'https://github.com/kefranabg/readme-md-generator/issues'
+        },
+        engines: {
+          npm: '>=5.5.0',
+          node: '>=9.3.0'
+        }
+      }
+      utils.getPackageJson.mockReturnValueOnce(Promise.resolve(packgeJsonInfos))
+      childProcess.execSync.mockReturnValue(
+        'https://github.com/kefranabg/readme-md-generator.git'
+      )
+
+      const projectInfos = await getProjectInfos()
+
+      expect(projectInfos).toEqual({
+        name: 'readme-md-generator',
+        description: 'CLI that generates beautiful README.md files.',
+        version: '0.1.3',
+        author: 'Franck Abgrall',
+        repositoryUrl: 'https://github.com/kefranabg/readme-md-generator',
+        homepage: 'https://github.com/kefranabg/readme-md-generator',
+        contributingUrl:
+          'https://github.com/kefranabg/readme-md-generator/issues',
+        githubUsername: 'kefranabg',
+        engines: {
+          npm: '>=5.5.0',
+          node: '>=9.3.0'
+        },
+        licenseName: 'MIT',
+        licenseUrl:
+          'https://github.com/kefranabg/readme-md-generator/blob/master/LICENSE',
+        documentationUrl:
+          'https://github.com/kefranabg/readme-md-generator#readme',
+        isGithubRepos: true,
+        usage: undefined,
+        testCommand: undefined
+      })
+    })
   })
 })
