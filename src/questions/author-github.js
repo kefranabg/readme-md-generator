13d16646f572e2afebd79344e9c256c7a204e7a8
Brian Douglas <bdougie@users.noreply.github.com>
2019-09-12 07:07:42 -0700

    Capitalize the H
    Apologies for open a tedious PR, but I stumbled on to this project after seeing the maintainer tweet out this getting added to the GitHub Package Registry. 
    
    Per the GitHub design guidelines, the "H" should always be capitalized. 
    
    Thanks for sharing and maintaing this project!

--- src/questions/author-github.js
+++ src/questions/author-github.js
@@ -2,7 +2,7 @@ const { cleanSocialNetworkUsername } = require('../utils')
 
 module.exports = projectInfos => ({
   type: 'input',
-  message: '👤  Github username (use empty value to skip)',
+  message: '👤  GitHub username (use empty value to skip)',
   name: 'authorGithubUsername',
   default: projectInfos.githubUsername,
   filter: cleanSocialNetworkUsername
