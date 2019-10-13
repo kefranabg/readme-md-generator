c6bc9526c3db8660a43e305a0b306913aba33c16
Franck <abgrallkefran@gmail.com>
2019-10-13 18:56:10 +0200

    :white_check_mark: Update tests

--- src/questions/author-website.spec.js
+++ src/questions/author-website.spec.js
@@ -1,4 +1,7 @@
+const fetch = require('node-fetch')
 const askAuthorWebsite = require('./author-website')
+
+jest.mock('node-fetch')
 
 describe('askAuthorWebsite', () => {
   it('should return correct question format', () => {
@@ -14,5 +17,38 @@ describe('askAuthorWebsite', () => {
         name: 'authorWebsite'
       })
     )
+  })
+
+  it('should return a new website url if user changes its github username with the previous question', async () => {
+    const blog = 'https://www.new-website-url.com/'
+    const projectInfos = {
+      githubUsername: 'kefranabg',
+      authorWebsite: 'https://www.franck-abgrall.me/'
+    }
+
+    fetch.mockReturnValueOnce(
+      Promise.resolve({
+        json: () => Promise.resolve({ blog })
+      })
+    )
+
+    const result = await askAuthorWebsite(projectInfos).default({
+      authorGithubUsername: 'newGitHubUsername'
+    })
+
+    expect(result).toEqual(blog)
+  })
+
+  it('should return project infos website url if github username did not change', async () => {
+    const projectInfos = {
+      githubUsername: 'kefranabg',
+      authorWebsite: 'https://www.franck-abgrall.me/'
+    }
+
+    const result = await askAuthorWebsite(projectInfos).default({
+      authorGithubUsername: 'kefranabg'
+    })
+
+    expect(result).toEqual(projectInfos.authorWebsite)
   })
 })
