f3a24486cc7e487eb45e23ebe58aa768cbf5c9c2
Franck <abgrallkefran@gmail.com>
2019-11-27 17:01:53 +0100

    :bug: Fix version badge label when contains _ or -

--- src/clean-context.js
+++ src/clean-context.js
@@ -4,14 +4,18 @@
  * @param {Object} context
  */
 module.exports = context => {
+  const cleanBadgeText = text => text.replace(/-/g, '--').replace(/_/g, '__')
+
   // Why doing this?
   // See https://github.com/kefranabg/readme-md-generator/pull/141
   const licenseName = context.licenseName
     .replace(/-/g, '--')
     .replace(/_/g, '__')
+  const projectVersion = cleanBadgeText(context.projectVersion)
 
   return {
     ...context,
-    licenseName
+    licenseName,
+    projectVersion
   }
 }
