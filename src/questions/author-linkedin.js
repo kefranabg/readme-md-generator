9c971b08b960c84421593011b4f93500951fa251
stech11 (SoftOrange Tech) <108250871+stech11845@users.noreply.github.com>
2022-07-15 20:46:11 -0600

    Delete src directory

--- src/questions/author-linkedin.js
+++
@@ -1,8 +0,0 @@
-const { cleanSocialNetworkUsername } = require('../utils')
-
-module.exports = () => ({
-  type: 'input',
-  message: '💼  LinkedIn username (use empty value to skip)',
-  name: 'authorLinkedInUsername',
-  filter: cleanSocialNetworkUsername
-})
--- src/questions/project-version.js
+++
@@ -1,6 +0,0 @@
-module.exports = projectInfos => ({
-  type: 'input',
-  message: 'ℹ️  Project version (use empty value to skip)',
-  name: 'projectVersion',
-  default: projectInfos.version
-})
